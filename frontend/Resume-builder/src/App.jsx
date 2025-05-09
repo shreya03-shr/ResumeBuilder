72633179b07942d22c14d2c81f26e15339ff63b9
vibha-09 <vibhakoder@gmail.com>
2025-05-09 11:44:17 +0530

    Added Routes

--- frontend/Resume-builder/src/App.jsx
+++ frontend/Resume-builder/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import LandingPage from './pages/LandingPage';
+import Login from './pages/Auth/Login';
+import Signup from './pages/Auth/Signup';
+import Dashboard from './pages/Home/Dashboard';
+import EditResume from './pages/ResumeUpdate/EditResume';
 
 const App = () => {
     return (
-        <div>App</div>
-    )
-}
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signUp" element={<Signup />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/resume/:resumeId" element={<EditResume/>} />
+                </Routes>
+            </Router>
+        </div>
+    );
+};
 
 export default App
